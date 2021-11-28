36e15bdeffeed04f952f8cd71fb5db806f8c67d8
Tony <awalker780@gmail.com>
2021-11-28 11:56:32 -0500

    Implements logout endpoint

--- express_server.js
+++ express_server.js
@@ -20,22 +20,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const userDatabase = {};
-
-// / page
+// Home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 // /urls page
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 // /urls/new page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 // Tiny URL Creating 
@@ -57,6 +61,12 @@ app.post("/login", (req, res) => {
   res.redirect(`/urls`);
 });
 
+// Logging Out 
+app.post("/logout", (req, res) => {
+  delete res.cookie("username", req.body["username"]);
+  res.redirect(`/urls`);
+});
+
 // Updating URL
 app.post("/urls/:shortURL/update", (req, res) => {
   delete urlDatabase[req.params.shortURL]
@@ -67,7 +77,11 @@ app.post("/urls/:shortURL/update", (req, res) => {
 
 // /urls/TinyURL page
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   req.params.shortURL = templateVars.shortURL;
   res.render("urls_show", templateVars);
 });
@@ -85,4 +99,3 @@ app.listen(PORT, ()=> {
 const generateRandomString = () => {
   return Math.random().toString(20).substring(2,8);
 };
-
