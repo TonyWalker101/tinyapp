304ca42fdfde9145f1adccc49352990f81b7dd77
Tony <awalker780@gmail.com>
2021-12-03 22:06:35 -0500

    Makes comments more clear

--- express_server.js
+++ express_server.js
@@ -1,26 +1,27 @@
-// Express Package
+// =========== Server Setup =======================================
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
-//Bcrypt Package
 const bcrypt = require("bcrypt");
 
-// Body Parser Package
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Cookie Session Package
 const cookieSession = require("cookie-session");
 app.use(cookieSession({
   name: "session",
   keys: ["key1", "key2"]
 }));
 
-// Setting template engine to EJS
 app.set("view engine", "ejs");
 
-//  Helper Functions
+app.listen(PORT, ()=> {
+  console.log(`App listening on Port: ${PORT}`);
+});
+
+// =========== Helper Functions ===================================
 
 const { 
   getUserByEmail, 
@@ -30,7 +31,7 @@ const {
   getUser
 } = require("./helper");
 
-// DBs
+// =========== Databases ==========================================
 
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "abc"},
@@ -45,13 +46,14 @@ const userDatabase = {
   }
 };
 
+// =========== GET Requests =======================================
 
 // Home page
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls/new");
 });
 
-// No user found page
+// /userNotFound page
 app.get("/usernotfound", (req, res) => {
 
   
@@ -90,19 +92,14 @@ app.get("/urls", (req, res) => {
 
 // /urls/new page
 app.get("/urls/new", (req, res) => {
-
-  if (req.session.user_id) {
-
-    const user = getUser(userDatabase, req.session.user_id);
-    
+  
+  const user = getUser(userDatabase, req.session.user_id);
+      
     const templateVars = {
       user: user
     };
-
-    return res.render("urls_new", templateVars);
-  }
-
-  return res.redirect("/usernotfound");
+  
+  return res.render("urls_new", templateVars);
 });
 
 // /register page
@@ -126,99 +123,6 @@ app.get("/login", (req, res) => {
   };
 
   res.render("login", templateVars);
-});
-
-
-// Registering New User
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-
-  randomID = generateRandomString();
-  const newUser1 = {};
-  
-  if (getUserByEmail(email, userDatabase)) {
-    return res.status(400).send("Email already registered!");
-  }
-
-  newUser1["id"] = randomID
-  newUser1["email"] = req.body["email"];
-  newUser1["password"] = bcrypt.hashSync(req.body["password"], 10);
-  req.session.user_id =  newUser1["id"];
-
-  userDatabase[randomID] = newUser1;
-  
-  res.redirect(`/urls`);
-});
-
-
-// Tiny URL Creating 
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  const cookie = req.session.user_id;
-
-  urlDatabase[shortURL] = {longURL: req.body["longURL"], userID: cookie};
-
-  res.redirect(`/urls/${shortURL}`);
-});
-
-
-// Deleting URL
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const cookie = req.session.user_id;
-  const shortURL = req.params.shortURL;
-  
-  if (cookie && urlDatabase[shortURL].userID === cookie) {
-    delete urlDatabase[shortURL];
-    return res.redirect("/urls"); 
-  }
-
-  return res.redirect("/usernotfound");
-});
-
-// Logging into our app 
-app.post("/login", (req, res) => {
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (getUserByEmail(email, userDatabase)) {
-
-    const user = getUserByEmail(email, userDatabase);
-
-    if (userPasswordMatches(user, password)) {
-      req.session.user_id = user.id;
-      return res.redirect("/urls");
-    }
-  }
-
-  return res.status(403).send("Email and password combination does not match our records!");
-});
-
-// Logging Out 
-app.post("/logout", (req, res) => {
-
-  req.session = null;
-  res.redirect(`/login`);
-
-});
-
-// Updating URL
-app.post("/urls/:shortURL/update", (req, res) => {
-  
-  const cookie = req.session.user_id;
-  const shortURL = req.params.shortURL;
-
-  if (cookie && urlDatabase[shortURL].userID === cookie) {
-    delete urlDatabase[req.params.shortURL]
-  
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = {longURL: req.body["longURL"], userID: cookie};
-  
-    return res.redirect(`/urls/${shortURL}`);
-  }
-  
-  return res.redirect("/usernotfound");
-
 });
 
 // /urls/TinyURL page
@@ -251,6 +155,93 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.listen(PORT, ()=> {
-  console.log(`Example app listening on port ${PORT}`);
+// =========== POST Requests ======================================
+
+// Registering New User
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+
+  randomID = generateRandomString();
+  const newUser1 = {};
+  
+  if (getUserByEmail(email, userDatabase)) {
+    return res.status(400).send("Email already registered!");
+  }
+
+  newUser1["id"] = randomID
+  newUser1["email"] = req.body["email"];
+  newUser1["password"] = bcrypt.hashSync(req.body["password"], 10);
+  req.session.user_id =  newUser1["id"];
+
+  userDatabase[randomID] = newUser1;
+  
+  res.redirect(`/urls`);
+});
+
+// Tiny URL Creating 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const cookie = req.session.user_id;
+
+  urlDatabase[shortURL] = {longURL: req.body["longURL"], userID: cookie};
+
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// Deleting URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const cookie = req.session.user_id;
+  const shortURL = req.params.shortURL;
+  
+  if (cookie && urlDatabase[shortURL].userID === cookie) {
+    delete urlDatabase[shortURL];
+    return res.redirect("/urls"); 
+  }
+
+  return res.redirect("/usernotfound");
+});
+
+// Logging in
+app.post("/login", (req, res) => {
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (getUserByEmail(email, userDatabase)) {
+
+    const user = getUserByEmail(email, userDatabase);
+
+    if (userPasswordMatches(user, password)) {
+      req.session.user_id = user.id;
+      return res.redirect("/urls");
+    }
+  }
+
+  return res.status(403).send("Email and password combination does not match our records!");
+});
+
+// Logging Out 
+app.post("/logout", (req, res) => {
+
+  req.session = null;
+  res.redirect(`/login`);
+
+});
+
+// Editing URL
+app.post("/urls/:shortURL/update", (req, res) => {
+  
+  const cookie = req.session.user_id;
+  const shortURL = req.params.shortURL;
+
+  if (cookie && urlDatabase[shortURL].userID === cookie) {
+    delete urlDatabase[req.params.shortURL]
+  
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = {longURL: req.body["longURL"], userID: cookie};
+  
+    return res.redirect(`/urls/${shortURL}`);
+  }
+  
+  return res.redirect("/usernotfound");
 });
